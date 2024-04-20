59d2734e7b6b66dba20b4674e101b44c28d511d0
Saurav Upadhyay <sauravup041103@gmail.com>
2024-04-20 06:05:45 +0530

    added go

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h2>Task Management System</h2>
+
+<div>
+    <h1>All Tasks</h1>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.title }}
+      </li>
+    </ul>
   </div>
+
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      tasks: []
+    }
+  },
+  mounted(){
+    axios.get('http://www.localhost:8000/api/tasks')
+    .then(response => {
+      this.tasks = response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching:', error)
+    });
   }
 }
 </script>
+
+<style scoped>
+h2{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
